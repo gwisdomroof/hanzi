9fdb6f3fc442b07273de779ed529d516c00b539b
Yang Yuntao <yangyuntao@meicai.cn>
2016-06-05 10:36:56 +0800

    修改汉子录入，细节需要优化

--- frontend/views/hanzi-split/component.php
+++ frontend/views/hanzi-split/component.php
@@ -13,39 +13,57 @@ $this->title = Yii::t('frontend', '部件集', [
 $this->params['breadcrumbs'][] = ['label' => Yii::t('frontend', 'Hanzis'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
+<style type="text/css">
+    .index {
+        display: inline-block;
+        width: 2em;
+        position: absolute;
+        margin: 5px;
+    }
+    .component-item {
+        width: 30px;
+        height: 30px;
+        font-size: 24px;
+        display: inline-table;
+        border: 1px solid #eee;
+        vertical-align: middle;
+        margin: 0;
+        padding: 0px;
+        border-collapse: collapse;;
+    }
+</style>
 <div class="hanzi-split-index">
 	<div class="col-sm-offset-1 col-sm-10">
 		<div style="margin-bottom:10px;">
 		    <div class="col-sm-8">
 		        <input type="text" id="ids" class="form-control col-sm-6" placeholder="请点选部件...">
 		    </div>
-		    <button type="button" id="ids-clear" class="btn">清空</button> 
+		    <button type="button" id="ids-clear" class="btn btn-primary">清空</button>
 	    </div>
 
-        <table class="component-table table table-striped table-bordered table-center">
-            <tbody>
+        <table class="table">
             <?php foreach ($components as $stock_num => $stock_array): ?>
                 <tr>
-                <td style="text-align: center; vertical-align:middle; "><?=$stock_num?></td>
-                <td style="word-wrap: break-word; word-break: normal;  ">
-                <?php 
-                $i = 1;
-                foreach ($stock_array as $key => $value) {
-                    if(mb_strlen($key,'utf-8') == 1) {
-                        echo "<span class='component-item' value='" . $value . "'>";
-                        echo $key;
-                    } else {
-                        echo "<span>";
-                        $path =  '/img/components/' . $key . '.png';
-                        echo "<img class='component-img' src='$path' alt='$value'>";
-                    }                  
-                    echo "</span>";
-                } ?>
-                </td>
+                    <td><span><?=$stock_num?></span></td>
+                    <td>
+                    <div>
+                    <?php
+                    $i = 1;
+                    foreach ($stock_array as $key => $value) {
+                        if(mb_strlen($key,'utf-8') == 1) {
+                            echo "<span class='component-item' value='" . $value . "'>";
+                            echo $key;
+                        } else {
+                            echo "<span>";
+                            $path =  '/img/components/' . $key . '.png';
+                            echo "<img class='component-item' src='$path' alt='$value'>";
+                        }
+                        echo "</span>";
+                    } ?>
+                    </div>
+                    </td>
                 </tr>
             <?php endforeach; ?>
-            </tbody>
         </table>
     </div>
 
