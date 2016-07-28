b1403d7b99d18e735dd2dd022eb974e63493be57
Xiandu <liuhao.ict@gmail.com>
2016-07-28 16:52:21 +0800

    台湾异体字乱码

--- frontend/views/layouts/twyitizi.php
+++ frontend/views/layouts/twyitizi.php
@@ -6,19 +6,17 @@ use yii\helpers\Html;
 \frontend\assets\FrontendAsset::register($this);
 ?>
 
-<?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="zh-TW">
 
 <head>
-    <meta charset="big5"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <META http-equiv="content-type" content="text/html; charset=big5">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <title><?php echo Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-    <?php echo Html::csrfMetaTags() ?>
+    <?php //$this->head() ?>
+    <?php //echo Html::csrfMetaTags() ?>
 </head>
 
 <?php echo $content ?>
 
 </html>
-<?php $this->endPage() ?>
