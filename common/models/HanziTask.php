a4943d21d39947a6c0fee8d95be440929247fa83
Xiandu <liuhao.ict@gmail.com>
2016-12-26 14:10:02 +0800

    修改最大页面

--- common/models/HanziTask.php
+++ common/models/HanziTask.php
@@ -139,7 +139,7 @@ class HanziTask extends \yii\db\ActiveRecord
         $gaoliSplitOffset = 1021;   # 高丽异体字第一页为1022
         # 异体字输入时的空白页面
         $inputEmptyPages = json_decode(Yii::$app->get('keyStorage')->get('frontend.input-empty-pages', null, false));
-        for ($i = 1; $i < $maxPageNumber; $i++) {
+        for ($i = 1; $i <= $maxPageNumber; $i++) {
             # 如果是异体字输入，则需要去掉空白页面
             if ($type == self::TYPE_INPUT && in_array($i . '', $inputEmptyPages)) {
                 continue;
